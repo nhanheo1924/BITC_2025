1aac7d0d2c686944ba626b5f7b3fd4791709eac5
nhanheo1924 <nhannguyenthien43@gmail.com>
2025-03-23 20:43:54 +0700

    Initial commit for BITC_2025

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main {
         d=4;
         e=5;
         f=6;
-        System.out.println(a + "\n" + b + "\n" + c + "\n" + d + "\n" + e + "\n" + f);
+        System.out.println("so thu nhat "+ a + "\n" + "so thu hai "+b + "\n" +"so thu ba "+ c + "\n"+"so thu tu "+ + d + "\n" +"so thu nam "+ e + "\n"+"so thu sau " + f);
         //bai3
         int a1 = 3;
         int b1 = 6;
